92671d582d4c42dddcabed6a11d881e9b3cb6c82
FluxCapacitor2 <31071265+FluxCapacitor2@users.noreply.github.com>
2025-01-01 02:31:29 -0500

    Copy sitemaps to `.vercel/output/static` when using Vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,12 +4,38 @@ import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel";
 import icon from "astro-icon";
 import { defineConfig } from "astro/config";
+import { copyFile, readdir } from "node:fs/promises";
+import { join, resolve } from "node:path";
+import { fileURLToPath } from "node:url";
 import rehypePrettyCode from "rehype-pretty-code";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.bswanson.dev",
-  integrations: [mdx(), sitemap(), tailwind(), icon()],
+  integrations: [
+    mdx(),
+    sitemap(),
+    tailwind(),
+    icon(),
+    process.env.VERCEL
+      ? {
+          // https://github.com/withastro/adapters/issues/445
+          name: "copy-sitemap-to-vercel-output-dir",
+          hooks: {
+            "astro:build:done": async ({ dir }) => {
+              const distDir = fileURLToPath(dir);
+              const staticDir = resolve(".vercel/output/static");
+              const files = await readdir(distDir);
+              for (const sitemap of files.filter((it) =>
+                it.includes("sitemap")
+              )) {
+                copyFile(join(distDir, sitemap), join(staticDir, sitemap));
+              }
+            },
+          },
+        }
+      : undefined,
+  ],
   adapter: process.env.VERCEL ? vercel() : undefined,
   markdown: {
     syntaxHighlight: false, // Handle syntax highlighting using rehype-pretty-code instead of the built-in solution
